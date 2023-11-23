95f32eb6083f64f61db7b14c358732416ad41061
DESKTOP-J2R3IU9\Papie <arekrasztar@gmail.com>
2023-11-23 17:23:57 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using ConsoleApp3;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Numerics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+internal class Program
+{
+  
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+
+        using (var httpClient = new HttpClient())
+        {
+            httpClient.BaseAddress = new Uri("https://localhost:7028/");
+
+
+            var responseTask = httpClient.GetAsync("WeatherForecast");
+            responseTask.Wait();
+
+            var result = responseTask.Result;
+            Console.WriteLine(result.IsSuccessStatusCode);
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsStringAsync();
+                readTask.Wait();
+
+                var weather = readTask.Result;
+                
+
+                var weatherForcasts = JsonConvert.DeserializeObject<List<WeatherForcast>>(weather);
+
+                Console.WriteLine(weatherForcasts[0].Date);
+
+                foreach (var item in weatherForcasts)
+                {
+                   
+                    Console.Write(item.Date + " ");
+                    Console.Write(item.TemperatureC + " ");
+                    Console.WriteLine(item.Summary + " ");
+
+                }
+            }
+        }       
+    }
+} 
